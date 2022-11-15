fbd79f7ff92eb3d42357d7839b22ba295677a2f7
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-11-15 15:39:13 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.51 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[] {};
+        List<int> list = new List<int> ();
+        for(int i = 0; i <= n; i++) {
+            if(i % 2 == 1) {
+                list.Add(i);
+            }
+        }
+        answer = list.ToArray();
+        return answer;
+    }
+}
