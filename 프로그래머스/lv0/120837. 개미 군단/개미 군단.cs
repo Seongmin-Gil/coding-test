4681eb2088aecc8565e00bd6a5d2cac48439a623
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-11-22 20:19:28 +0900

    [level 0] Title: 개미 군단, Time: 0.15 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int a = hp / 5;
+        int b = (hp % 5) / 3;
+        int c = ((hp % 5) % 3) / 1;
+        answer = a + b + c;
+        return answer;
+    }
+}
