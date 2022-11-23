0ff1dfec3642d17f36194f0512f44f86a14cba2c
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-11-23 20:05:56 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.15 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public long solution(int balls, int share) {
+        if(share == 0) {
+            return 1;
+        }
+        long answer = solution(balls -1, share -1);
+        answer *= balls;
+        answer /= share;
+        return answer;
+    }
+}
