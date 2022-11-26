e9460e89e564a8a5878583082c0adff5dfa17e1b
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-11-26 16:08:54 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.24 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int[,] solution(int[] num_list, int n) {
+        int[,] answer = new int[num_list.Length / n, n];
+        int count = 0;
+        int index = 0;
+        for(int i = 0; i < num_list.Length; i ++) {
+            answer[index, count] = num_list[i];
+            count ++;
+            if(count == n ) {
+                index ++;
+                count = 0;
+            }
+        }
+        return answer;
+    }
+}
