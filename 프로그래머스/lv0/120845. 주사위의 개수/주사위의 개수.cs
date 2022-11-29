a198b15c93616f4bb238a9e0da553bd84a9e4ce6
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-11-29 18:50:43 +0900

    [level 0] Title: 주사위의 개수, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 0;
+        int x = box[0] / n;
+        int y = box[1] / n;
+        int z = box[2] / n;
+        answer = x * y * z; 
+        return answer;
+    }
+}
