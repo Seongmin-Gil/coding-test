a502ffbc698dca93fbebc3163280c3767ac746d6
Gil Seongmin(Mark) <ksm65501@gmail.com>
2023-02-13 19:22:13 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = 0;
+        for(int i = 0; i < array.Length; i ++)
+        {
+            if(array[i] > max)
+            {
+                max = array[i];
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+            
+        }
+        return answer;
+    }
+}
